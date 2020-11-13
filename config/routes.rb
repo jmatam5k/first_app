f860dae3195ac97419af4623da188e97dbff91ae
Jennifer Matamala <katana@MacBook-Pro-de-Jennifer.local>
2020-11-12 21:43:06 -0300

    se agrega navbar con la estructura requerida

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'pages/one'
   get 'pages/two'
   get  'pages/three' # Agregar un archivo llamado three.html.erb a la carpeta views del controlador pages
+  
   root 'pages#one' #Setear la página one como página de inicio
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
